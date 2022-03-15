e6a5c65da11f1fe9fb843a0153e4a9fc753bbfcb
seyrinn <79247355+seyrinn@users.noreply.github.com>
2022-03-15 16:06:45 +0900

    bug fix

--- Assets/U#Script/SyncedObject.cs
+++ Assets/U#Script/SyncedObject.cs
@@ -8,21 +8,31 @@ using VRC.Udon;
 public class SyncedObject : UdonSharpBehaviour
 {
     
-    [UdonSynced] private string _syncedLogData;
+    [UdonSynced] private string _syncedLogData = "default"; //string null 비교시 or 동기화시 문제있는듯?
     public LogPanel logPanel;
     private bool isLogSaved = false;
+
+    private bool isFirstSync = true;
+
+    private string lastlog = null;
+
+    public string savedRemoteLog = "";
+    public string savedLocalLog = "";
+
     
     public void getOwner(){
         Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-        logPanel.Log(this, "GetOwnerSucess");
-
     }
 
-    
+
+    /* 기존 유저들 저장되어있는 첫 로그값은 무시 */
+    /* 나중에 들어올 유저들 로그값은 무시하면 안됨*/
+    public void setIsFirstSync(bool value){
+        isFirstSync = value;
+    }
 
     public void sendLog(string data){
         localLog(data);
-
         if(!isLogSaved){
             _syncedLogData = data;
             isLogSaved = true;
@@ -35,24 +45,47 @@ public class SyncedObject : UdonSharpBehaviour
         //logPanel.Log(this, "OnPreSerialization");
         isLogSaved = false;
     }
-    public void localLog(string data){
-        logPanel.PrintLog(data);
-    }
 
     public override void OnDeserialization() {
-        if(_syncedLogData == "")
+        //logPanel.PrintLog("OnDeserialization : " + this.gameObject.name);
+        if(lastlog == _syncedLogData || _syncedLogData == "default")
             return;
+
+        lastlog = _syncedLogData;
+        /* 첫 싱크시 저장되어있는 다른 유저들 로그들 나오는거 방지*/
+        if(isFirstSync){
+            isFirstSync = false;
+            return;
+        }
+        //logPanel.PrintLog("on deserialization 통과 : " + this.gameObject.name);
         remoteLog(_syncedLogData);
     }
     
+
+    public void localLog(string data){
+        logPanel.PrintLog(data);
+    }
 
     public void remoteLog(string data){
         logPanel.PrintLog(data);
     }
 
     public void resetLog(){
-        _syncedLogData="";
+        Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+        _syncedLogData = "default";
         RequestSerialization();
+        //logPanel.LogError(this, this.gameObject.name + " resetcomplete");
+    }
+
+
+    public void getlog(){
+        logPanel.PrintLog(this.gameObject.name + " : " + _syncedLogData);
+    }
+    private void Update() {
+        
+        if(Input.GetKeyDown(KeyCode.F10)){
+            getlog();
+        }
     }
 
 }
