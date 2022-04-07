b8c4be838dcda054021ff5cd86de02505ec5e693
KIBA <kjh030529@gmail.com>
2022-04-07 17:47:02 +0900

    Move Element draw responsibility (DebugEditor.cs -> EditorUtil.cs) Use EditorUtil.cs for DebugEditor.cs

--- Assets/Chamchi/Editor/EditorUtil.cs
+++ Assets/Chamchi/Editor/EditorUtil.cs
@@ -1,13 +1,69 @@
 using System;
 using UnityEditor;
+using UnityEngine;
 
 namespace CHAMCHI.BehaviourEditor
 {
-    public class EditorUtil : Editor
+    public static class EditorUtil
     {
-        private void OnEnable()
+
+        public static void DrawTitle(string Title)
         {
-            throw new NotImplementedException();
+            EditorGUILayout.LabelField("", GUI.skin.window, GUILayout.Height(25));
+            EditorGUI.LabelField(GUILayoutUtility.GetLastRect(), $"   <size=15><b>{Title}</b></size>", GUI.skin.label);
+        }
+        
+        public static bool DrawFoldoutTitle(bool b, string Title)
+        {
+            EditorStyles.foldout.richText = true;
+            if (GUILayout.Button("", GUI.skin.window, GUILayout.Height(25)))
+                b = !b;
+            EditorGUI.LabelField(GUILayoutUtility.GetLastRect(), $"   <size=15><b>{Title}</b></size>", GUI.skin.label);
+
+            return b;
+        }
+        
+        public static void MenuBox(string Title, Action contant, ContentStyle style)
+        {
+            var origin_font = GUI.skin.label.font;
+            DrawTitle(Title);
+            GUI.skin.label.font = style.font;
+            
+            EditorGUILayout.BeginHorizontal(GUI.skin.box);
+                EditorGUILayout.Space(10);
+                EditorGUILayout.BeginVertical();
+                    contant.Invoke();
+                EditorGUILayout.EndVertical();
+            EditorGUILayout.EndHorizontal();
+            
+            GUI.skin.label.font = origin_font;
+        }
+        
+        public static bool FoldoutMenuBox(string Title, bool b, Action contant, ContentStyle style)
+        {
+            var origin_font = GUI.skin.label.font;
+            b = DrawFoldoutTitle(b, Title);
+
+            if (!b) return b;
+            
+            GUI.skin.label.font = style.font;
+            
+            EditorGUILayout.BeginHorizontal(GUI.skin.box);
+            EditorGUILayout.Space(10);
+            EditorGUILayout.BeginVertical();
+            contant.Invoke();
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.EndHorizontal();
+            
+            GUI.skin.label.font = origin_font;
+            
+            return b;
+        }
+        
+        public static void MenuBox(string Title, Action contant)
+        {
+            DrawTitle(Title);
+            contant.Invoke();
         }
     }
 }
