13f9ad100f6fbb05b94bd4d2c356daaea0210695
KIBA <kjh030529@gmail.com>
2022-04-07 16:10:10 +0900

    Make EditorUtil.cs for Refactor DebugEditor.cs Inspector Drawer

---
+++ Assets/Chamchi/Editor/EditorUtil.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEditor;
+
+namespace CHAMCHI.BehaviourEditor
+{
+    public class EditorUtil : Editor
+    {
+        private void OnEnable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
